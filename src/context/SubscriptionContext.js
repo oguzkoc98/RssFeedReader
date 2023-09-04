95291fa393188de1da6ad05c861ffb7701e744c3
Oğuz Koç <oguzkoc98@gmail.com>
2023-09-05 01:00:19 +0300

    RssFeedReader

--- src/context/SubscriptionContext.js
+++ src/context/SubscriptionContext.js
@@ -1,19 +1,47 @@
-import React, {createContext, useContext, useState} from 'react';
+import React, {createContext, useContext, useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SubscriptionContext = createContext();
 
 function SubscriptionProvider({children}) {
   const [subscriptionData, setSubscriptionData] = useState([]);
 
-  function addSubscription(url, name) {
-    const index = subscriptionData.findIndex(item => item.url === url);
-    if (index !== -1) {
-      setSubscriptionData(subscriptionData.filter(item => item.url !== url));
-    } else {
-      setSubscriptionData([...subscriptionData, {url, name}]);
-    }
-  }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await AsyncStorage.getItem('subscriptionData');
+        if (data) {
+          setSubscriptionData(JSON.parse(data));
+        }
+      } catch (error) {
+        console.error('Veri alınamadı:', error);
+      }
+    };
+
+    getData();
+  }, []);
   console.log(subscriptionData);
+
+  const addSubscription = async (url, name) => {
+    const updatedData = [...subscriptionData];
+    const index = updatedData.findIndex(item => item.url === url);
+    if (index !== -1) {
+      updatedData.splice(index, 1);
+    } else {
+      updatedData.push({url, name});
+    }
+
+    try {
+      await AsyncStorage.setItem(
+        'subscriptionData',
+        JSON.stringify(updatedData),
+      );
+      setSubscriptionData(updatedData);
+    } catch (error) {
+      console.error('Veri kaydedilemedi:', error);
+    }
+  };
+
   return (
     <SubscriptionContext.Provider value={{subscriptionData, addSubscription}}>
       {children}
